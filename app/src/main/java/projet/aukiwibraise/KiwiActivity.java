210f14c4f730394e9221ae55fe712b7ecd9f531e
Nashunn <boullet.nicolas@gmail.com>
2017-03-21 23:03:02 +0100

    Ajout du listener sur l'ecran d'accueil.

--- app/src/main/java/projet/aukiwibraise/KiwiActivity.java
+++ app/src/main/java/projet/aukiwibraise/KiwiActivity.java
@@ -1,13 +1,33 @@
 package projet.aukiwibraise;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class KiwiActivity extends AppCompatActivity {
+    //instanciation
+    LinearLayout accueil_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiwi);
+        //On recupere le layout de l'accueil
+        accueil_layout=(LinearLayout) findViewById(R.id.activity_kiwi);
+        //On rajoute le listener
+        accueil_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(KiwiActivity.this, "This is my Toast message!", Toast.LENGTH_LONG).show();
+                /*
+                AJOUT : a ajouter pour passer d'une activité a l'autre
+
+                Intent intent=new Intent(MainActivity.this,Second.class);
+                finish();
+                startActivity(intent);
+                */
+            }
     }
 }
